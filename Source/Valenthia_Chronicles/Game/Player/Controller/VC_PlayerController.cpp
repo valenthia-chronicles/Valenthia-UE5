a15add2ef1d289d6235c1882bb0be0641b203661
Skertchshell <skertchshell@proton.me>
2024-05-07 19:32:41 +0200

    feat: setup the player controller and add basic inputs

--- Source/Valenthia_Chronicles/Game/Player/Controller/VC_PlayerController.cpp
+++ Source/Valenthia_Chronicles/Game/Player/Controller/VC_PlayerController.cpp
@@ -1,4 +1,74 @@
 // MIT License - Copyright (c) 2024 Valenthia Chronicles
 
 #include "VC_PlayerController.h"
+#include "Engine/LocalPlayer.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+
+
+AVC_PlayerController::AVC_PlayerController()
+{
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	bEnableClickEvents = true;
+}
+
+void AVC_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Recover Input Mapping Context
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+}
+
+void AVC_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	// Bind each input action to a function
+	if(UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		// Jumping
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &AVC_PlayerController::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &AVC_PlayerController::Jump);
+
+		// Moving
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AVC_PlayerController::Move);
+	} 
+}
+
+
+void AVC_PlayerController::Move(const FInputActionValue& Value)
+{
+	if (GetCharacter() != nullptr)
+	{
+		const FVector2D MovementVector = Value.Get<FVector2D>();
+		
+		// Find out which way is forward
+		const FRotator Rotation = GetCharacter()->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// Get forward vector
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		// Add movement direction
+		GetCharacter()->AddMovementInput(ForwardDirection, MovementVector.Y);
+		GetCharacter()->AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void AVC_PlayerController::Jump(const FInputActionValue& Value)
+{
+	if (GetCharacter() != nullptr)
+	{
+		GetCharacter()->Jump();
+	}
+}
+
 
--- Source/Valenthia_Chronicles/Game/Player/Controller/VC_PlayerController.h
+++ Source/Valenthia_Chronicles/Game/Player/Controller/VC_PlayerController.h
@@ -6,9 +6,38 @@
 #include "GameFramework/PlayerController.h"
 #include "VC_PlayerController.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
 
 UCLASS(Abstract, Blueprintable)
 class VALENTHIA_CHRONICLES_API AVC_PlayerController : public APlayerController
 {
 	GENERATED_BODY()
+	
+public:
+	AVC_PlayerController();
+
+	// Default Input Mapping Context
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+
+	// Jump Input Action
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> JumpAction;
+
+	// Move Input Action
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> MoveAction;
+	
+protected:
+	virtual void BeginPlay() override;
+	
+	virtual void SetupInputComponent() override;
+	
+private:
+	// Input Events
+	void Move(const FInputActionValue& Value);
+	void Jump(const FInputActionValue& Value);
 };
